0b6491c7be96c338fed9bcaf3e3af2c8bcf67d2e
MartinSimonKT <martin-s@centrum.cz>
2026-01-03 16:50:05 +0100

    0.0.0

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { env } from "./env";
 
 declare global {
@@ -6,13 +6,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const log: Prisma.PrismaClientOptions["log"] =
-  env.NODE_ENV === "development" ? ["warn", "error"] : ["error"];
+const log = env.NODE_ENV === "development" ? ["warn", "error"] : ["error"];
 
-export const db =
-  globalThis.prisma ||
-  new PrismaClient({
-    log,
-  });
+export const db = globalThis.prisma || new PrismaClient({ log });
 
 if (env.NODE_ENV !== "production") globalThis.prisma = db;
